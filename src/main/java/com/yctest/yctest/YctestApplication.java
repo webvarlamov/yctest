8149365ca0809f855c6e15b0af688b70bf7759fe
roman <web.varlamov@yande.ru>
2021-02-23 00:17:20 +0300

    Init

---
+++ src/main/java/com/yctest/yctest/YctestApplication.java
@@ -0,0 +1,13 @@
+package com.yctest.yctest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YctestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YctestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yctest/yctest/YctestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yctest.yctest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YctestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
